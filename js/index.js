82b216a28436cf9f7f6381caaf6e90dbab6336ac
Mateo Pujol <137231452+StrangeHuman-Developer@users.noreply.github.com>
2023-12-30 04:24:14 -0300

    patoteamox3

--- js/index.js
+++ js/index.js
@@ -1,41 +1,43 @@
-let motivationText = ''
+// ... (código CSS)
+
+let motivationText = '';
 
 const startTime = () => {
     let today = new Date();
     let hourNow = today.getHours();
     let minutesNow = today.getMinutes();
     let secondsNow = today.getSeconds();
-    h = checkTime(hourNow);
-    m = checkTime(minutesNow);
-    s = checkTime(secondsNow);
+    let h = checkTime(hourNow);
+    let m = checkTime(minutesNow);
+    let s = checkTime(secondsNow);
     document.getElementById("clock").innerHTML = h + ":" + m + ":" + s;
-    var t = setTimeout(function() {
-        startTime()
-    }, 500);
 
-    if (hourNow > 6 && hourNow < 12) {
-        motivationText = '¡Buenos días! Hoy vas a pegar buenas balas en el cs.'
+    if (hourNow > 6 && hourNow < 11) {
+        motivationText = '¡Buenos días! Hoy vas a pegar buenas balas en el cs.';
     }
     if (hourNow > 12 && hourNow < 14) {
-        motivationText = 'Empeza a tirar bien los fb y los smokes.'
+        motivationText = 'Empeza a tirar bien los fb y los smokes.';
     }
     if (hourNow > 14 && hourNow < 19) {
-        motivationText = '¡Buenas tardes! #Anda a jugar al cs'
+        motivationText = '¡Buenas tardes! #Anda a jugar al cs';
     }
-    if (hourNow > 19 && hourNow <= 6) {
-        motivationText = '¡Buenas noches! Recordá pajearte y descansar bien.'
+    if (hourNow > 19 && hourNow <= 5) {
+        motivationText = '¡Buenas noches! Recordá pajearte y descansar bien.';
     }
-}
 
-const body = document.querySelector('body').addEventListener('load', startTime())
+    const motivationDiv = document.querySelector('#motivationDiv');
+    motivationDiv.innerHTML = motivationText;
 
+    var t = setTimeout(function() {
+        startTime();
+    }, 500);
+};
 
-const motivationDiv = document.querySelector('#motivationDiv')
-motivationDiv.innerHTML = motivationText
+window.addEventListener('load', startTime);
 
 function checkTime(i) {
     if (i < 10) {
-        i = "0" + i
-    }; // add zero in front of numbers < 10
+        i = "0" + i;
+    } // add zero in front of numbers < 10
     return i;
 }
