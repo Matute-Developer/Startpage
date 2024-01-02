b60b31d80b1fb9bb9d59b37878ad4d8fc3398ba9
Mateo Pujol <137231452+StrangeHuman-Developer@users.noreply.github.com>
2024-01-02 05:22:17 -0300

    patoteamo

--- js/index.js
+++ js/index.js
@@ -15,15 +15,16 @@ const startTime = () => {
     if (hourNow >= 5 && hourNow < 12) {
         motivationText = '¡Buenos días! Hoy vas a pegar buenas balas en el cs.';
     }
-    else if  (hourNow >= 12 && hourNow < 14) {
-        motivationText = 'Empeza a tirar bien los fb y los smokes.';
+    else if (hourNow >= 12 && hourNow < 14) {
+        motivationText = 'Empezá a tirar bien los fb y los smokes.';
     }
     else if (hourNow >= 14 && hourNow < 20) {
-        motivationText = '¡Buenas tardes! #Anda a jugar al cs';
+        motivationText = '¡Buenas tardes! #Andá a jugar al cs';
     }
-    else if (hourNow >= 20 && hourNow < 5) {
+    else {
         motivationText = '¡Buenas noches! Recordá pajearte y descansar bien.';
     }
+    
 
     const motivationDiv = document.querySelector('#motivationDiv');
     motivationDiv.innerHTML = motivationText;
